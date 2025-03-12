f701a27d744cd39d89b7b8f7640ad3e1e1f1c3ac
Jared-teck <emmanuelbrais1609@gmail.com>
2025-03-11 20:08:07 -0600

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,32 @@
+
+
+import discord
+from discord.ext import commands
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='$', intents=intents)
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hola, soy un bot {bot.user}!')
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send("Hi!")
+    elif message.content.startswith('$bye'):
+        await message.channel.send("\U0001f642")
+    else:
+        await message.channel.send(message.content)
+
+
+
+
+
+bot.run("MTM0NjY2NzE3NjM4NzQxMTk4OQ.GKHa-G.nba4zsE4WHnze2J_cHRGX98Vs8V65LPpXMsim8")
